3a10413743474a1ec7aebc227fddce03bfdb71a4
Anna Till <ic21b041@technikum-wien.at>
2023-01-15 14:10:21 +0100

    initial commit

---
+++ src/main/java/at/fhtw/fhtwbicmaintenancemonitor/FhtwBicMaintenanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.fhtwbicmaintenancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhtwBicMaintenanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FhtwBicMaintenanceMonitorApplication.class, args);
+    }
+
+}
